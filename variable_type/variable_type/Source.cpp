6d29cba5df21172c1ea67290644b533d5f1cbcca
ONZ1 <onzi1230@gmail.com>
2021-04-06 21:33:39 +0900

    Create Source.cpp

---
+++ variable_type/variable_type/Source.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int i_num_1 = 10;
+	int i_num_2 = 30;
+	int i_num_3 = 200;
+	int i_num_4 = 500;
+
+	printf("%d\n", i_num_1);
+	printf("%d\n", i_num_2);
+	printf("%d\n", i_num_3);
+	printf("%d\n", i_num_4);
+
+	fgetc(stdin);
+}
